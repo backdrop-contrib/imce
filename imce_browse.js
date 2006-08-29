f114f5c0b30b649384cd672d93c3d8e8c7eca6cb
ufku <ufku@9910.no-reply.drupal.org>
2006-08-29 19:28:56 +0000

    * Highlighting of new files

--- imce_browse.js
+++ imce_browse.js
@@ -1,46 +1,48 @@
 // $Id$
 addLoadEvent(imceStartBrowser);
 
-var selectedrow = null;
+var imceSelectedRow = null;
 
 function imceStartBrowser() {
-  var rows = $('bodytable').getElementsByTagName('tr');
-  var row;
+  var row, rows = $('bodytable').getElementsByTagName('tr');
+  var path = $('latest-file').value ? $('latest-file').value : (window.opener&&!$('imagepreview').innerHTML ? window.opener.imceRefererURL : null);
   for (var i=0; row = rows[i]; i++) {
-    if (!row.getAttribute('ipath')) continue;
+    if (path && path==row.getAttribute('ipath')) {
+      imceHighlight(row, 1);
+    }
     row.onmouseover = function() {addClass(this, 'rover')};
     row.onmouseout = function() {removeClass(this, 'rover')};
-    row.onclick = function() {
-      if (selectedrow) {
-        removeClass(selectedrow, 'rsel');
-      }
-      if (selectedrow==this) {
-        selectedrow =null;
-        $('imagepreview').innerHTML = '';
-      }
-      else {
-        $('imagepreview').innerHTML = '<a href="javascript: imceFinitor(\'' + this.getAttribute('ipath') + '\', ' + this.getAttribute('iw') + ', ' + this.getAttribute('ih') + ')"><img src="'+this.getAttribute('ipath')+'"></a>';
-        addClass(this, 'rsel');
-        selectedrow = this;
-      }
-    }
+    row.onclick = function() {imceHighlight(this);};
+  }
+}
+
+function imceHighlight(row, append) {
+  if (imceSelectedRow) {
+    removeClass(imceSelectedRow, 'rsel');
+  }
+  if (imceSelectedRow==row) {
+    imceSelectedRow =null;
+    $('imagepreview').innerHTML = '';
+  }
+  else {
+    var w = parseInt(row.getAttribute('iw'));
+    var h = parseInt(row.getAttribute('ih'));
+    var path = row.getAttribute('ipath');
+    addClass(row, 'rsel');
+    imceSelectedRow = row;
+    $('imagepreview').innerHTML = (append ? $('imagepreview').innerHTML : '') + (w&&h ? '<a href="javascript: imceFinitor(\'' + path + '\', ' + w + ', ' + h + ')"><img src="'+ path +'" width="'+ w +'" height="'+ h +'" /></a>' : '');
   }
 }
 
 function imceFinitor(path, w, h) {
-  if (!window.opener) return;
-  if (window.opener.imceRefererType=='image' && !(w&&h)) {
-    if (!confirm($('confirm-msg').value)) return;
+  if (!window.opener || !window.opener.imceRefererWin || (window.opener.imceRefererType=='image' && !(w&&h) && !confirm($('confirm-msg').value))) return;
+  window.opener.imceRefererField.value = path;
+  if (window.opener.imceRefererWin.document.forms[0].width) {
+    window.opener.imceRefererWin.document.forms[0].width.value = w;
   }
-  if (window.opener.imceRefererWin) {
-    window.opener.imceRefererField.value = path;
-    if (window.opener.imceRefererWin.document.forms[0].width) {
-      window.opener.imceRefererWin.document.forms[0].width.value = w;
-    }
-    if (window.opener.imceRefererWin.document.forms[0].height) {
-      window.opener.imceRefererWin.document.forms[0].height.value = h;
-    }
-    window.opener.imceRefererWin.focus();
+  if (window.opener.imceRefererWin.document.forms[0].height) {
+    window.opener.imceRefererWin.document.forms[0].height.value = h;
   }
+  window.opener.imceRefererWin.focus();
   window.close();
 }
