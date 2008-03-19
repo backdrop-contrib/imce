693dcf0735a1748cfce8aec9e033436163c0341d
ufku <ufku@9910.no-reply.drupal.org>
2008-03-19 02:37:30 +0000

    moved file id from cell to row

--- tpl/imce-file-list.tpl.php
+++ tpl/imce-file-list.tpl.php
@@ -6,8 +6,8 @@ $imce =& $imce_ref['imce'];
 <table id="file-list" class="files"><tbody><?php
 if ($imce['perm']['browse'] && !empty($imce['files'])) {
   foreach ($imce['files'] as $name => $file) {?>
-  <tr>
-    <td class="name" id="<?php print $raw = rawurlencode($file['name']); ?>"><?php print $raw; ?></td>
+  <tr id="<?php print $raw = rawurlencode($file['name']); ?>">
+    <td class="name"><?php print $raw; ?></td>
     <td class="size" id="<?php print $file['size']; ?>"><?php print format_size($file['size']); ?></td>
     <td class="width"><?php print $file['width']; ?></td>
     <td class="height"><?php print $file['height']; ?></td>
